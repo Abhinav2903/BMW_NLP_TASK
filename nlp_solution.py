dbe0c633e7103fff3bc92d0dde81cb16c882a32a
Abhinav2903 <abhinavpareek532@gmail.com>
2023-05-15 17:19:52 +0200

    check for all the models with substring done

--- nlp_solution.py
+++ nlp_solution.py
@@ -44,7 +44,7 @@ class NlpPromptConverter:
 
     # define the function to extract relevant information from the prompt provided
     def extract_info(self):
-        model_type = None
+        model_type = []
         formulas = []
         date = None
         
@@ -60,19 +60,30 @@ class NlpPromptConverter:
 
     # Define the funtion for extracting model type codecs
     def extract_model_type(self,model_type):
-        for code, description in model_type_codes.items():
+        # check for the exact match in the prompt provided
+        for code,description in model_type_codes.items():
             if description.lower() in self.prompt.lower():
-                model_type = code
-                break
+                model_type.append(code)
+                break;
+        # check for the length of the model type if it is 0 then check for the specific substring provided in the prompt
+        if(len(model_type)==0):
+            for code,description in model_type_codes.items():    
+                words = description.lower().split() 
+                if (any( word in self.prompt.lower() for word in words)):
+                    print('xxxx', description)
+                    model_type.append(code)
         return model_type;
 
     # define the function for extracting date
     def extract_date(self,date):
+
+        # check for the date that have start,mid or end in the prompt
         matches = re.findall(r"\b(?:start|mid|end) of \w+\s+\d{4}\b", self.prompt)
         month_mapping = {name.lower(): index for index, name in enumerate(calendar.month_name) if name}
         if(matches):
             if matches:
                 date = matches[0]
+                # cases for the differnt type and synonym writing in the prompt
                 if 'start of' in date:
                     date = date.replace("start of","1st")
                 elif 'starting' in date:
@@ -82,20 +93,21 @@ class NlpPromptConverter:
                 elif 'end of' in date:
                     date = date.replace("end of", "")
                     month, year = date.split()
-                    # Get the last day of the month
+                    # Get the last day of the month as differnt months have different end date liske 27th, 28th,30th or 31st  
                     last_day = calendar.monthrange(int(year), month_mapping[month.lower()])[1]
                     # Replace the date with the month's last day
                     date = f"{last_day} {month} {year}"
                 elif 'ending' in date:
                     date = date.replace("ending", "")
                     month, year = date.split()
-                    # Get the last day of the month
+                     # Get the last day of the month as differnt months have different end date liske 27th, 28th,30th or 31st  
                     last_day = calendar.monthrange(int(year), month_mapping[month.lower()])[1]
                     # Replace the date with the month's last day
                     date = f"{last_day} {month} {year}"   
                 date_obj = parse(date, fuzzy=True)
                 date = date_obj.strftime("%Y-%m-%d")  
         else:
+            # check for the date that have normal dates like 1st,2nd or kind of numeric and word format in the prompt
             matches = re.findall(r"\b\d{1,2}(?:st|nd|rd|th)?\s+(?:of\s+)?\w+\s+\d{4}\b", self.prompt)
             if(matches):
                 date = matches[0]
@@ -109,7 +121,7 @@ class NlpPromptConverter:
 
     # Function to convert extracted information into a request body
     def create_request_body(self,model_type, formulas, date):
-        model_type, formulas, date = self.extract_info()
+        # model_type, formulas, date = self.extract_info()
 
         evaluated_formulas = []
         for formula in formulas:
